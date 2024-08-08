d8aa9a13849d90ba30aa75135b9070b29d239215
enes <enes@DESKTOP-N059GP9>
2024-08-08 17:08:51 +0300

    commit

---
+++ DirectExchange.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Create connection
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//We define the exchange that we define on the publisher side, also on the consumer side.
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+//Queuing
+string queueName = channel.QueueDeclare().QueueName;
+
+//Binding
+channel.QueueBind(queue: queueName,exchange: "direct-exchange-example",routingKey: "direct-queue-example");
+
+//Create consumer
+EventingBasicConsumer consumer = new (channel);
+
+//Consumer settings
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.ReadLine();
---
+++ DirectExchange.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Create connection
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Creating an exchange and define exchange type
+channel.ExchangeDeclare(exchange:"direct-exchange-example",type:ExchangeType.Direct);
+
+while (true)
+{
+    Console.WriteLine("Mesaj gir = ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+    //We publish the messages we receive from the console
+    channel.BasicPublish(exchange: "direct-exchange-example",routingKey:"direct-queue-example",body:byteMessage);
+
+}
+
+Console.ReadLine();
