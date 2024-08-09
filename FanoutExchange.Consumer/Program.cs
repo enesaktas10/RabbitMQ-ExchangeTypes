f5b2336c3188cfcdfbb111c259f9b74eb4ddecaf
enes <enes@DESKTOP-N059GP9>
2024-08-09 14:22:43 +0300

    Fanout Exchange commit

---
+++ FanoutExchange.Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "fanout-exchange-example",
+    type: ExchangeType.Fanout);
+
+Console.Write("Kuyruk Adi Gir = ");
+string _queueName = Console.ReadLine();
+
+channel.QueueDeclare(
+    queue: _queueName,
+    exclusive: false);
+
+channel.QueueBind(
+    queue:_queueName,
+    exchange: "fanout-exchange-example",
+    routingKey:string.Empty);
+
+EventingBasicConsumer consumer = new(channel);
+
+channel.BasicConsume(
+    queue:_queueName,
+    consumer:consumer,
+    autoAck:true);
+
+consumer.Received += (sender, e) =>
+{
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
+};
+
+Console.ReadLine();
---
+++ FanoutExchange.Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange:"fanout-exchange-example",
+    type:ExchangeType.Fanout);
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        routingKey:string.Empty,
+        body:message);
+
+}
+
+Console.Read();
